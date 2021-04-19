e2a923c2b66758a4c9999fad961b52febc4d3be2
zemdom <misiaze@gmail.com>
2021-04-19 13:14:27 +0200

    add data standarization and plot augmented data

--- data_splitting/dataset_splitter.py
+++ data_splitting/dataset_splitter.py
@@ -2,6 +2,7 @@ import os
 
 import numpy as np
 from tensorflow import keras
+import matplotlib.pyplot as plt
 
 from settings import PROCESSED_LABELS_FILEPATH, PROCESSED_DATASET_PATH
 
@@ -22,11 +23,30 @@ class DatasetSplitter:
     def prepare_dataset_split(self, dataset):
         # _, y_train_set, _, y_validate_set, _, y_test_set = self.split1(dataset)
         # _, _, y_train_set, _, y_validate_set, _, y_test_set = self.split2(dataset)
-        _, _, y_train_set, _, y_validate_set, _, y_test_set = self.split3(dataset)
+        data_generator, x_train_set, y_train_set, _, y_validate_set, _, y_test_set = self.split3(dataset)
+
+        for elem in x_train_set:
+            self.__plot_augmented_data(data_generator, elem)
 
         print(f'Train set size:\t{len(y_train_set)}')
         print(f'Validate set size:\t{len(y_validate_set)}')
         print(f'Test set size:\t{len(y_test_set)}')
+
+    @staticmethod
+    def __plot_augmented_data(data_generator, image):
+        image = np.array([image])
+        augment_iterator = data_generator.flow(image, batch_size=1)
+
+        fig, ax = plt.subplots(nrows=3, ncols=4, figsize=(15, 15))
+
+        for row in range(0, 3):
+            for column in range(0, 4):
+                image = augment_iterator.next()[0].astype('float64')
+
+                ax[row][column].imshow(image)
+                ax[row][column].axis('off')
+
+        fig.show()
 
     def split1(self, entries_list, shuffle_datasets=True, merge_datasets=False):
         print(f'{self.log_name} Splitting dataset into split1')
@@ -62,6 +82,10 @@ class DatasetSplitter:
 
         x_train_set, y_train_set, x_validate_set, y_validate_set, x_test_set, y_test_set = self.split1(
             dict_entries_list, shuffle_datasets, merge_datasets)
+
+        x_train_set = self.standardize(x_train_set)
+        x_validate_set = self.standardize(x_validate_set)
+        x_test_set = self.standardize(x_test_set)
 
         x_train_set = self.normalize(x_train_set)
         x_validate_set = self.normalize(x_validate_set)
@@ -128,6 +152,19 @@ class DatasetSplitter:
                                                                       width_shift_range=image_shift,
                                                                       zca_whitening=False)
         return data_generator
+
+    @staticmethod
+    def standardize(dataset, shift=False):
+        dataset_max = np.amax(dataset)
+        dataset_min = np.amin(dataset)
+
+        dataset = dataset - dataset_min
+        standardized_dataset = dataset / (dataset_max - dataset_min)
+
+        if shift:
+            standardized_dataset = standardized_dataset - 0.5
+
+        return standardized_dataset
 
     @staticmethod
     def normalize(dataset):
